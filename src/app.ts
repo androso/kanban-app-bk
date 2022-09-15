e1b5ca83f4a54bf754ca4c33b3bbc0d6eccc58a4
androso <androso421@gmail.com>
2022-09-15 17:24:45 -0600

    updating folder structure

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
-// We're using ES modules instead of commonJS, you can change this in package.json
-import express from 'express';
+
+const express = require("express") 
 const app = express();
 // You can set your port in the Secrets tab.
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
+	
 	res.send("Hello world!!");
-})
+});
 
 app.listen(PORT, () => {
 	console.log("app listening on port", PORT);
-})
+});
