9e1109b40054bebfdd22ec198c8b67854b2cb169
androso <androso421@gmail.com>
2022-09-17 08:46:25 -0600

    adding express-session + deserialize user Function

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,57 @@
 import express from "express";
 import authRouter from "./routes/auth/authRouter";
+import session from "express-session";
 import cors from "cors";
+import { findById } from "./helpers/helpers";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
+app.use(
+	session({
+		secret: "mySecretNoob",
+		resave: false,
+		saveUninitialized: false,
+		cookie: {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24,
+			secure: false,
+			sameSite: "strict",
+		},
+	})
+);
+// Deserialize user
+app.use((req, res, next) => {
+	if (req.session.authenticated && req.session.userId) {
+		findById(req.session.userId, (err, user) => {
+			if (err) {
+				console.log(err);
+				res.status(500).send("there was an error");
+			} else if (!user) {
+				console.log("there's no user with that id");
+				res.status(401).send("there's no user with that id");
+			} else {
+				req.user = user;
+				next();
+			}
+		});
+	} else {
+		next();
+	}
+});
 app.get("/", (req, res) => {
 	res.send("Hello world!!");
 });
 
 app.get("/profile", (req, res) => {
-	res.send("My profile! if you were redirected heFre, you are logged in!");
+	if (req.session.authenticated) {
+		res.send(
+			`My profile! if you were redirected heFre, you are logged in! ${req.user?.email}`
+		);
+	} else {
+		res.send("You are not logged in");
+	}
 });
 
 app.use(authRouter);
