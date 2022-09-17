fcab88a25d3b07755f12e4924c4aa10cad353771
androso <androso421@gmail.com>
2022-09-17 08:46:05 -0600

    adding post handler to /login

--- src/routes/auth/authRouter.ts
+++ src/routes/auth/authRouter.ts
@@ -17,7 +17,36 @@ authRouter
 		res.send("login");
 	})
 	.post((req, res, next) => {
-		console.log("posting to /login");
+		const { email, password } = req.body;
+		console.log(email, password);
+		// check if user exists w that email
+		// if not alert
+		// if it does, check if password is correct
+		// if it doesn't, reload,
+		// if it does, set req.session.authenticated to true.
+		// add the deserializej middleware to root router, so that we get access to the user object in req.user.
+		findUserByEmail(email, async (err: Error | null, user: User | null) => {
+			if (err) {
+				console.log("there was an error");
+				res.status(500).send("there was an error");
+			} else if (!user) {
+				console.log("there's no user registered with this email");
+				res.status(401).send("there's no user registered with this email");
+			} else {
+				const passwordsMatch = await bcrypt.compare(password, user.password);
+				if (!passwordsMatch) {
+					console.log("Wrong password");
+					res.status(401).send("Wrong password");
+				} else {
+					// Things inside req.session object persists between requests. Why?
+					req.session.authenticated = true;
+					req.session.userId = user.id;
+					
+					console.log(req.user);
+					res.redirect("/profile");
+				}
+			}
+		});
 	});
 
 authRouter
@@ -27,13 +56,12 @@ authRouter
 	})
 	.post(async (req, res) => {
 		const { email, password } = req.body;
-
 		// check if we already have a user with that email
 		try {
 			const user = await userExists(email);
 			if (user) {
 				console.log("user already exists!");
-				res.redirect("/users/login");
+				res.status(400).send("user already exists!");
 			} else {
 				const userId = getNewId(users);
 				const salt = await bcrypt.genSalt(10);
@@ -49,7 +77,7 @@ authRouter
 				writeJSONFile(usersFilename, users);
 				console.log("user created correctly");
 				console.log(newUser);
-				res.redirect("/login");
+				res.status(201).send("user created correctly");
 			}
 		} catch (e) {
 			console.error(e);
