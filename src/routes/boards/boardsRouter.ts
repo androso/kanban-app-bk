f06718a9596a6bd86f9c495f72a2058fe9a0d31a
androso <androso421@gmail.com>
2022-10-14 12:44:22 -0600

    feat: adding endpoint to get full information of a board

--- src/routes/boards/boardsRouter.ts
+++ src/routes/boards/boardsRouter.ts
@@ -6,11 +6,13 @@ import { Board } from "../../sql-orm/entity/Board";
 import { Status } from "../../sql-orm/entity/Status";
 import { initialStatuses } from "../../helpers/helpers";
 import { Task } from "../../sql-orm/entity/Task";
+import { User } from "../../sql-orm/entity/User";
+
 const BoardRepository = AppDataSource.getRepository(Board);
 const BoardToStatusRepository = AppDataSource.getRepository(BoardToStatus);
 const StatusRepository = AppDataSource.getRepository(Status);
 const TaskRepository = AppDataSource.getRepository(Task);
-
+const UserRepository = AppDataSource.getRepository(User);
 boardsRouter
 	.route("/")
 	.get(async (req, res) => {
@@ -21,13 +23,15 @@ boardsRouter
 		res.json(boards);
 	})
 	.post(async (req, res) => {
-		// For now we assume we have an authenticating middleware at app.ts that returns earlier if they're not logged in
 		const { title, description } = req.body;
 		if (req.session.user) {
+			const userFromDB = await UserRepository.findOneByOrFail({
+				id: req.session.user.id,
+			});
 			const newBoard = BoardRepository.create({
 				title,
 				description,
-				user: req.session.user,
+				user: userFromDB,
 			});
 			try {
 				// we should also link this board with the initial statuses
@@ -70,6 +74,7 @@ boardsRouter
 boardsRouter
 	.route("/:boardId")
 	.delete(async (req, res) => {
+		// TODO: make sure cascade things are set up correctly
 		const { boardId } = req.params;
 		const board = await BoardRepository.findOneBy({ id: Number(boardId) });
 		if (board?.user === req.session.userId) {
@@ -108,61 +113,38 @@ boardsRouter
 		}
 	});
 
-// boardsRouter.route("/:boardId").get(async (req, res) => {
-// 	const { boardId } = req.params;
-// 	const board = await BoardRepository.findOneBy({ id: Number(boardId) });
-// 	if (board?.user === req.session.userId) {
-// 		try {
-// 			const statusesIds = await BoardToStatusRepository.findBy({
-// 				board: Number(boardId),
-// 			});
-// 			const statuses = await Promise.all(
-// 				statusesIds.map((statusId) => StatusRepository.findOneBy(statusId))
-// 			);
-
-// 			const tasks = await TaskRepository.findBy({ board: Number(boardId) });
-
-// 			const tasksWithStatus = tasks.map((task) => {
-// 				console.log({ taskStatus: task.status });
-// 				const status = statuses.find((status) => status?.id === task.status);
-// 				return { ...task, status: status };
-// 			});
-// 			console.log({ tasksWithStatus, statuses, tasks });
-// 			res.json({
-// 				...board,
-// 				statuses,
-// 				tasks: tasksWithStatus,
-// 			});
-// 		} catch (e) {
-// 			if (e instanceof Error) {
-// 				console.error(e);
-// 				res.sendStatus(500);
-// 			}
-// 		}
-// 	}
-// });
-
-// boardsRouter.route("/:boardId/statuses").get(async (req, res) => {
-// 	const { boardId } = req.params;
-// 	const board = await BoardRepository.findOneBy({ id: Number(boardId) });
-// 	if (board?.user === req.session.userId) {
-// 		try {
-// 			const statusesIds = await BoardToStatusRepository.findBy({
-// 				board: Number(boardId),
-// 			});
-// 			const statuses = await Promise.all(
-// 				statusesIds.map((statusId) => StatusRepository.findOneBy(statusId))
-// 			);
-// 			res.json(statuses);
-// 		} catch (e) {
-// 			if (e instanceof Error) {
-// 				console.error(e);
-// 				res.sendStatus(500);
-// 			}
-// 		}
-// 	} else {
-// 		res.status(401);
-// 	}
-// });
-
+boardsRouter.route("/:boardId").get(async (req, res) => {
+	const { boardId } = req.params;
+	const board = await BoardRepository.findOneByOrFail({
+		id: Number(boardId),
+	});
+	if (board && req.session.user) {
+		if (board.userId === req.session.user.id) {
+			try {
+				// find the statuses related to this board
+				const boardStatuses = (
+					await BoardToStatusRepository.find({
+						relations: ["status"],
+						where: { boardId: board.id },
+					})
+				).map((boardToStatus) => {
+					return boardToStatus.status;
+				});
+				const boardTasks = await TaskRepository.findBy({
+					boardId: board.id,
+				});
+				res.json({
+					...board,
+					statuses: boardStatuses,
+					tasks: boardTasks,
+				});
+			} catch (e) {
+				if (e instanceof Error) {
+					console.error(e);
+					res.sendStatus(500);
+				}
+			}
+		}
+	}
+});
 export default boardsRouter;
