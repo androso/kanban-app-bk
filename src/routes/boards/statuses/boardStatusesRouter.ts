e58070077844e51c9979fee9bf6e463fa55e7a07
androso <androso421@gmail.com>
2022-10-18 20:26:16 -0600

    Feature: deleting a status and all its related tasks

--- src/routes/boards/statuses/boardStatusesRouter.ts
+++ src/routes/boards/statuses/boardStatusesRouter.ts
@@ -1,6 +1,13 @@
 import { Router } from "express";
-import { BoardRepository, StatusRepository } from "../boardsRouter";
+import { Task } from "../../../sql-orm/entity/Task";
+import { AppDataSource } from "../../../sql-orm/data-source";
+import {
+	BoardRepository,
+	StatusRepository,
+	TaskRepository,
+} from "../boardsRouter";
 const boardStatusesRouter = Router({ mergeParams: true });
+
 boardStatusesRouter.route("/").post(async (req, res) => {
 	const { title, color } = req.body;
 	const { boardId } = req.params as { boardId: string };
@@ -25,7 +32,7 @@ boardStatusesRouter.route("/").post(async (req, res) => {
 						// status is already created and connected to the board, doesn't make sense to repeat it
 						res.sendStatus(409);
 					} else {
-                        // we link the status to the board
+						// we link the status to the board
 						board.statuses.push(statusAlreadyInDB);
 						await BoardRepository.save(board);
 						res.sendStatus(201);
@@ -46,6 +53,71 @@ boardStatusesRouter.route("/").post(async (req, res) => {
 			}
 		} else {
 			res.sendStatus(400);
+		}
+	}
+});
+boardStatusesRouter.route("/:statusId").delete(async (req, res) => {
+	const statusIdNumber = Number(req.params.statusId);
+	const { boardId } = req.params as { boardId: string; statusId: string };
+	const boardIdNumber = Number(boardId);
+	console.log(typeof boardIdNumber, typeof statusIdNumber);
+	if (!isNaN(statusIdNumber) && !isNaN(boardIdNumber)) {
+		const status = await StatusRepository.findOne({
+			where: { id: statusIdNumber },
+			relations: ["boards"],
+		});
+		console.log(status);
+		if (status) {
+			// check if this is the last board subscribed to this status
+			if (status.boards.length === 1) {
+				// in that case we delete the status from the statuses table
+				// and we delete it from the junction table
+				// we also delete all of the tasks that are from this board and have this status id
+				const board = await BoardRepository.findOne({
+					where: { id: boardIdNumber },
+					relations: ["statuses"],
+				});
+				if (!board) {
+					res.sendStatus(404);
+				} else {
+					board.statuses = board.statuses.filter(
+						(status) => status.id !== statusIdNumber
+					);
+					await BoardRepository.save(board);
+					await StatusRepository.delete(statusIdNumber);
+					await AppDataSource.createQueryBuilder()
+						.delete()
+						.from(Task)
+						.where("statusId = :id", { id: statusIdNumber })
+						.andWhere("boardId = :id", { id: boardIdNumber })
+						.execute();
+					res.sendStatus(204);
+				}
+			} else {
+				// we just delete the connection between this board and the status
+				// and any task from this board that has that status
+				const board = await BoardRepository.findOne({
+					where: { id: boardIdNumber },
+					relations: ["statuses"],
+				});
+				if (!board) {
+					res.sendStatus(404);
+				} else {
+					board.statuses = board.statuses.filter(
+						(status) => status.id !== statusIdNumber
+					);
+					await BoardRepository.save(board);
+					await AppDataSource.createQueryBuilder()
+						.delete()
+						.from(Task)
+						.where("statusId = :id", { id: statusIdNumber })
+						.andWhere("boardId = :id", { id: boardIdNumber })
+						.execute();
+					res.sendStatus(204);
+				}
+			}
+		} else {
+			res.sendStatus(404);
 		}
 	}
 });
